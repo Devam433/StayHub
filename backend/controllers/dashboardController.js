0c88dd27b2973d04d0f21f0ce13eb9a5fb921924
Devam <devam433@gmail.com>
2024-12-30 12:58:25 +0530

    Add getTenantDashboard

--- backend/controllers/dashboardController.js
+++ backend/controllers/dashboardController.js
@@ -1,4 +1,5 @@
 import { ownerDashboardModel } from "../models/ownerDashboardModel.js";
+import { tenantDashboardModel } from "../models/tenantDashboardModel.js";
 
 export async function getOwnerDashboard(req,res,next) {
   try {
@@ -14,10 +15,16 @@ export async function getOwnerDashboard(req,res,next) {
   }
 }
 
-export async function getTanentDashboard(req,res) {
+export async function getTenantDashboard(req,res) {
   try {
-    
+    const response = await tenantDashboardModel.findOne({tenantId:req.user.id})
+    if(!response) {
+      res.status(404).json({success:false,message:'Not found'})
+    }
+    return res.status(200).json(response)
   } catch (error) {
-    
+    console.log('Error occured at getOwnerDahboard',error);
+    const statusCode = error.statusCode || 500;
+    res.status(statusCode).json({success:false,message:error.message || 'Unexpected error!',error})
   }
 }
